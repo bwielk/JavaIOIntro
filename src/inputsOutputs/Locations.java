ba0c4ab6ec9640a2b1aa27695c4828bd1b738773
bwielk <bwielk@gmail.com>
2018-04-22 15:16:14 +0200

    Applying BufferedWriter to the locations main() method

--- src/inputsOutputs/Locations.java
+++ src/inputsOutputs/Locations.java
@@ -1,55 +1,21 @@
 package inputsOutputs;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 public class Locations implements Map<Integer, Location> {
 
-    private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
+    private static Map<Integer, Location> locations = new LinkedHashMap<Integer, Location>();
 
     public static void main(String[] args) throws IOException {
 
-//        Map<String, Integer> tempExit = new HashMap<String, Integer>();
-//
-//        //DESERT
-//        tempExit = new HashMap<String, Integer>();
-//        tempExit.put("W", 2);
-//        tempExit.put("N", 3);
-//        tempExit.put("E", 4);
-//        tempExit.put("S", 5);
-//        locations.put(1, new Location(1, "Sunny and dry desert", tempExit));
-//        //GRAVEYARD
-//        tempExit = new HashMap<String, Integer>();
-//        tempExit.put("N", 3);
-//        tempExit.put("E", 1);
-//        tempExit.put("S", 5);
-//        locations.put(2, new Location(2, "Abandoned city full of zombies", tempExit));
-//        //LUCKY_FOREST
-//        tempExit = new HashMap<String, Integer>();
-//        tempExit.put("W", 2);
-//        tempExit.put("E", 4);
-//        tempExit.put("S", 1);
-//        locations.put(3, new Location(3, "Magical fields of clovers", tempExit));
-//        //HELL
-//        tempExit = new HashMap<String, Integer>();
-//        tempExit.put("S", 5);
-//        locations.put(4, new Location(4, "Condemned area in the Hell", tempExit));
-//        //HEAVEN
-//        tempExit = new HashMap<String, Integer>();
-//        tempExit.put("W", 2);
-//        tempExit.put("N", 1);
-//        locations.put(5, new Location(5, "Blessings in the Heaven", tempExit));
-
-        try (FileWriter localFile = new FileWriter("locations.txt");
-             FileWriter directionsFile = new FileWriter("directions.txt")) {
-            for (Location location : locations.values()) {
-                localFile.write(location.getLocationID() + "\t"
+        try (BufferedWriter locationsFile = new BufferedWriter(new FileWriter("locations.txt"));
+             BufferedWriter directionsFile = new BufferedWriter(new FileWriter("directions.txt"))){
+            for(Location location : locations.values()) {
+                locationsFile.write(location.getLocationID() + "\t"
                         + location.getDescription() + "\t"
                         + location.getExits().size() + "\n");
-                for (String direction : location.getExits().keySet()) {
+                for(String direction : location.getExits().keySet()) {
                     directionsFile.write(location.getLocationID() + "\t"
                             + direction + "\t"
                             + location.getExits().get(direction) + "\n");
@@ -74,6 +40,7 @@ public class Locations implements Map<Integer, Location> {
 
         //static initialisation
     static {
+
         try ( Scanner scanner = new Scanner(new BufferedReader(new FileReader("locations.txt")));){
             scanner.useDelimiter("\t");
             while (scanner.hasNextLine()) {
